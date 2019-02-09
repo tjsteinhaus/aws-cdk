7a0deb54807ec140b2fe700765a05225805839c4
tjsteinhaus <tjsteinhaus@gmail.com>
2019-02-09 12:39:40 -0600

    Exporting ENV Vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Echo out a Secret
-echo "$AWS_DEFAULT_OUTPUT"
+# Export Env Vars
+export AWS_DEFAULT_OUTPUT = "$AWS_DEFAULT_OUTPUT"
+export AWS_ACCESS_KEY_ID = "$AWS_ACCESS_KEY_ID"
+export AWS_DEFAULT_REGION = "$AWS_DEFAULT_REGION"
+export AWS_REGION = "$AWS_REGION"
+export AWS_SECRET_ACCESS_KEY = "$AWS_SECRET_ACCESS_KEY"
 
 # Install NPM Modules
 npm install
