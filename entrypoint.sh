c5cf90963a0470b6e09e4b1baacb74b305387cee
tjsteinhaus <tjsteinhaus@gmail.com>
2019-02-09 13:17:26 -0600

    Hide touch and .env creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@
 # Export Env Vars
 echo "Create .env file"
 
-touch .env
-echo "AWS_DEFAULT_OUTPUT=$AWS_DEFAULT_OUTPUT" >> .env
-echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
-echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" >> .env
-echo "AWS_REGION=$AWS_REGION" >> .env
-echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
+# touch .env
+# echo "AWS_DEFAULT_OUTPUT=$AWS_DEFAULT_OUTPUT" >> .env
+# echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
+# echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" >> .env
+# echo "AWS_REGION=$AWS_REGION" >> .env
+# echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
 
 # Install NPM Modules
 echo "Install NPM Modules"
