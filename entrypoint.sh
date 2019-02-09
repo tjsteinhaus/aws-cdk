5e3f6102757c60cda0c422fbafd04c1028190b55
tjsteinhaus <tjsteinhaus@gmail.com>
2019-02-09 13:20:01 -0600

    Will CDK deploy outside of npm script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-# Export Env Vars
-echo "Create .env file"
-
-# touch .env
-# echo "AWS_DEFAULT_OUTPUT=$AWS_DEFAULT_OUTPUT" >> .env
-# echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
-# echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" >> .env
-# echo "AWS_REGION=$AWS_REGION" >> .env
-# echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
-
 # Install NPM Modules
 echo "Install NPM Modules"
 npm install
 
 # Run Deploy Script
 sh -c "npm run deploy -- $*"
+
+cdk deploy
